0b2202b85fb7de47cea3ee689c03e11852afe867
Celal <kir.celal@outlook.com>
2026-01-30 23:37:23 +0100

    Fix Moving

--- game/src/LevelParser.cpp
+++ game/src/LevelParser.cpp
@@ -177,13 +177,14 @@ LevelParser::LevelParser(std::string filename, Level* level, MainWindow* mw)
 
             int pos_x           = v.second.get<int>("position_x", 0);
             int pos_y           = v.second.get<int>("position_y", 0);
-            float jumpForceX    = v.second.get<float>("jump_force_x", 0.0);
-            float jumpForceY    = v.second.get<float>("jump_force_y", 0.0);
-            float moveForceX    = v.second.get<float>("move_force_x", 0.0);
-            float moveForceY    = v.second.get<float>("move_force_y", 0.0);
-            float maxVelRun     = v.second.get<float>("max_run_velocity", 0.0);
-            float maxVelFall    = v.second.get<float>("max_fall_velocity", 0.0);
-            float maxJumpHeight = v.second.get<float>("max_jump_height", 0.0);
+            // Standardwerte aus ActorForces, falls nicht in XML angegeben
+            float jumpForceX    = v.second.get<float>("jump_force_x", 0.0f);
+            float jumpForceY    = v.second.get<float>("jump_force_y", -440.0f);
+            float moveForceX    = v.second.get<float>("move_force_x", 800.0f);
+            float moveForceY    = v.second.get<float>("move_force_y", 0.0f);
+            float maxVelRun     = v.second.get<float>("max_run_velocity", 400.0f);
+            float maxVelFall    = v.second.get<float>("max_fall_velocity", 160.0f);
+            float maxJumpHeight = v.second.get<float>("max_jump_height", 150.0f);
 
             // TODO Jation & Pascal: the textureDataSet muust be changed. it must be dependent on the
             // Create new actor
